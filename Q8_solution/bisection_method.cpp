139876ae20440da9593c04a0acb6211cfa80543c
George Akerman <george_akerman@hotmail.co.uk>
2022-03-01 14:45:31 +0000

    Added q8 solution (not run)

---
+++ Q8_solution/bisection_method.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <stdexcept>
+
+#include "bisection_method.h"
+
+constexpr int max_iterations = 1000;
+
+template <typename T>
+double bisection_method(double y_target, double left, double right, double epsilon, T functor)
+{
+	double evaluate_left = T(left)-y_target;
+	double evaluate_right = T(right) - y_target;
+	int iterations_counter = 0;
+	do
+	{
+		double mid_point = left + right / 2;
+		double evaluate_mid = T(mid_point) - y_target;
+		if (abs(evaluate_mid - y_target < epsilon))
+		{
+			return mid_point;
+		}
+		if (evaluate_left < 0 == evaluate_mid < 0)
+		{
+			left = mid_point;
+			evaluate_left = evaluate_mid;
+		}
+		else
+		{
+			right = mid_point;
+			evaluate_right = evaluate_mid;
+		}
+		iterations_counter++;
+	}
+	while (iterations_counter < max_iterations);
+	throw std::runtime_error("Max iterations exceeded.");
+}
---
+++ Q8_solution/bisection_method.h
@@ -0,0 +1,2 @@
+template <typename T>
+double bisection_method(double y_target, double left, double right, double epsilon, T functor);
