f56b091544cf4747a05cd4dfa0c1b0f5e872e6a0
George Akerman <george_akerman@hotmail.co.uk>
2022-03-08 17:34:13 +0000

    Using price variable

--- Q8_Client/Q8_Client.cpp
+++ Q8_Client/Q8_Client.cpp
@@ -13,7 +13,7 @@ int main()
     std::cout << "The inplied volatility of a call option with Expiry: 0.25, Strike price: 95, initial price: 100, r:10% is: \n)";
 
     BlackScholesCallFunctor functor(T, K, S, r);
-    std::cout << interval_bisection<BlackScholesCallFunctor>(13.6953, 100, 10, 0.00000001, functor);
+    std::cout << interval_bisection<BlackScholesCallFunctor>(price, 100, 10, 0.00000001, functor);
 }
 
 
